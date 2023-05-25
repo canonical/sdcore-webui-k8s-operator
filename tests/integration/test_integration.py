d5beb3e5c73bb30d550a9e39d2de6df6d80ab09a
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-05-25 11:05:12 -0400

    Implementation of the webui operator (#2)

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,6 +1,70 @@
+#!/usr/bin/env python3
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 
-def test_dummy_pass():
-    assert True
+import logging
+from pathlib import Path
+
+import pytest
+import yaml
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = METADATA["name"]
+DATABASE_APP_NAME = "mongodb-k8s"
+
+
+async def _deploy_database(ops_test):
+    """Deploy a MongoDB."""
+    await ops_test.model.deploy(
+        DATABASE_APP_NAME,
+        application_name=DATABASE_APP_NAME,
+        channel="5/edge",
+        trust=True,
+    )
+
+
+@pytest.fixture(scope="module")
+@pytest.mark.abort_on_fail
+async def build_and_deploy(ops_test):
+    """Build the charm-under-test and deploy it."""
+    charm = await ops_test.build_charm(".")
+    resources = {
+        "webui-image": METADATA["resources"]["webui-image"]["upstream-source"],
+    }
+    await ops_test.model.deploy(
+        charm,
+        resources=resources,
+        application_name=APP_NAME,
+        trust=True,
+    )
+    await _deploy_database(ops_test)
+
+
+@pytest.mark.abort_on_fail
+async def test_given_charm_is_built_when_deployed_then_status_is_blocked(
+    ops_test,
+    build_and_deploy,
+):
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="blocked",
+        timeout=1000,
+    )
+
+
+@pytest.mark.abort_on_fail
+async def test_relate_and_wait_for_active_status(
+    ops_test,
+    build_and_deploy,
+):
+    await ops_test.model.add_relation(
+        relation1=f"{APP_NAME}:database", relation2=f"{DATABASE_APP_NAME}"
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        timeout=1000,
+    )
--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -1,7 +1,171 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import unittest
+from io import StringIO
+from unittest.mock import Mock, patch
 
-class TestCharm:
-    def test_dummy_pass(self):
-        assert True
+from ops import testing
+from ops.model import ActiveStatus, WaitingStatus
+
+from charm import WebuiOperatorCharm
+
+
+def read_file_content(path: str) -> str:
+    """Reads a file and returns as a string.
+
+    Args:
+        path (str): path to the file.
+
+    Returns:
+        str: content of the file.
+    """
+    with open(path, "r") as f:
+        content = f.read()
+    return content
+
+
+class TestCharm(unittest.TestCase):
+    @patch(
+        "charm.KubernetesServicePatch",
+        lambda charm, service_name, ports: None,
+    )
+    def setUp(self):
+        self.namespace = "whatever"
+        self.harness = testing.Harness(WebuiOperatorCharm)
+        self.harness.set_model_name(name=self.namespace)
+        self.addCleanup(self.harness.cleanup)
+        self.harness.begin()
+
+    def _database_is_available(self) -> str:
+        database_url = "http://6.6.6.6"
+        database_username = "banana"
+        database_password = "pizza"
+        database_relation_id = self.harness.add_relation("database", "mongodb")
+        self.harness.add_relation_unit(
+            relation_id=database_relation_id, remote_unit_name="mongodb/0"
+        )
+        self.harness.update_relation_data(
+            relation_id=database_relation_id,
+            app_or_unit="mongodb",
+            key_values={
+                "username": database_username,
+                "password": database_password,
+                "uris": database_url,
+            },
+        )
+        return database_url
+
+    @patch("ops.model.Container.push")
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_not_written_when_database_is_created_then_config_file_is_written(
+        self,
+        patch_exists,
+        patch_push,
+    ):
+        patch_exists.side_effect = [True, False]
+        self.harness.set_can_connect(container="webui", val=True)
+
+        self.harness.charm._on_database_created(event=Mock(uris="1.9.11.4:1234,5.6.7.8:1111"))
+
+        expected_config_file_content = read_file_content("tests/unit/expected_webui_cfg.json")
+
+        patch_push.assert_called_with(
+            path="/etc/webui/webuicfg.conf",
+            source=expected_config_file_content,
+        )
+
+    @patch("ops.model.Container.push")
+    @patch("ops.model.Container.pull")
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_content_doesnt_match_when_database_changed_then_content_is_updated(
+        self,
+        patch_exists,
+        patch_pull,
+        patch_push,
+    ):
+        patch_exists.side_effect = [True, True]
+        patch_pull.return_value = StringIO("Obviously different content")
+        self.harness.set_can_connect(container="webui", val=True)
+
+        self.harness.charm._on_database_created(event=Mock(uris="1.9.11.4:1234,5.6.7.8:1111"))
+
+        expected_config_file_content = read_file_content("tests/unit/expected_webui_cfg.json")
+
+        patch_push.assert_called_with(
+            path="/etc/webui/webuicfg.conf",
+            source=expected_config_file_content,
+        )
+
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_is_written_when_pebble_ready_then_pebble_plan_is_applied(
+        self,
+        patch_exists,
+    ):
+        patch_exists.return_value = True
+
+        self._database_is_available()
+
+        self.harness.container_pebble_ready(container_name="webui")
+
+        expected_plan = {
+            "services": {
+                "webui": {
+                    "override": "replace",
+                    "command": "./webconsole/webconsole -webuicfg /etc/webui/webuicfg.conf",
+                    "startup": "enabled",
+                    "environment": {
+                        "GRPC_GO_LOG_VERBOSITY_LEVEL": "99",
+                        "GRPC_GO_LOG_SEVERITY_LEVEL": "info",
+                        "GRPC_TRACE": "all",
+                        "GRPC_VERBOSITY": "debug",
+                        "CONFIGPOD_DEPLOYMENT": "5G",
+                    },
+                }
+            },
+        }
+
+        updated_plan = self.harness.get_container_pebble_plan("webui").to_dict()
+
+        self.assertEqual(expected_plan, updated_plan)
+
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_is_written_when_pebble_ready_then_status_is_active(
+        self, patch_exists
+    ):
+        patch_exists.return_value = True
+
+        self._database_is_available()
+
+        self.harness.container_pebble_ready("webui")
+
+        self.assertEqual(self.harness.model.unit.status, ActiveStatus())
+
+    @patch("ops.model.Container.exists")
+    def test_given_config_file_is_not_written_when_pebble_ready_then_status_is_waiting(
+        self, patch_exists
+    ):
+        patch_exists.return_value = False
+
+        self._database_is_available()
+
+        self.harness.container_pebble_ready("webui")
+
+        self.assertEqual(
+            self.harness.model.unit.status,
+            WaitingStatus("Waiting for config file to be written"),
+        )
+
+    @patch("ops.model.Container.exists")
+    def test_given_storage_not_attached_when_on_database_created_then_status_is_waiting(
+        self,
+        patch_exists,
+    ):
+        patch_exists.return_value = False
+        self.harness.set_can_connect(container="webui", val=True)
+
+        self.harness.charm._on_database_created(event=Mock(uris="1.9.11.4:1234,5.6.7.8:1111"))
+
+        self.assertEqual(
+            self.harness.model.unit.status, WaitingStatus("Waiting for storage to be attached")
+        )
