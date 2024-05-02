6fb6dd10f521464a51e9fdea8cc1cebb37074bdd
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-05-02 11:13:58 -0400

    chore: centralise metadata in charmcraft.yaml

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -14,7 +14,7 @@ from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 APP_NAME = METADATA["name"]
 DATABASE_APP_NAME = "mongodb-k8s"
 DATABASE_APP_CHANNEL = "6/beta"
